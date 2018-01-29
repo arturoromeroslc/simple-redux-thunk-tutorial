20d13a2ceda5bbe8e7d5b532eff842b0f9688645
Arturo Romero <artromero801@gmail.com>
2018-01-28 19:18:04 -0800

    playing with favicon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import Favicon from 'react-favicon';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import counter from './state/counter'
 import name from './state/name'
@@ -19,8 +20,11 @@ console.log(store.getState());
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider>
-      <Counter />
-      <Name />
+      <span>
+        <Favicon url="http://oflisback.github.io/react-favicon/public/img/github.ico" />
+        <Counter />
+        <Name />
+      </span>
     </MuiThemeProvider>
   </Provider>,
   document.getElementById('app')
--- src/view/couter/component.js
+++ src/view/couter/component.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Favicon from 'react-favicon';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class Counter extends React.Component {
@@ -11,6 +12,10 @@ class Counter extends React.Component {
     return (
       <span>
         <h1>This is the number {this.props.number}</h1>
+        {this.props.number > 1 ?
+          <Favicon url="https://www.npmjs.com/static/images/touch-icons/favicon-32x32.png" />
+          : <Favicon url="http://oflisback.github.io/react-favicon/public/img/github.ico" />
+        }
         <RaisedButton onClick={this.props.increase}>+</RaisedButton>
         <RaisedButton onClick={this.props.decrease}>-</RaisedButton>
       </span>
